01411f2b094f0cd7563c673e5bbd1fe692d3dbae
143Sangam143 <katwalsangam@gmail.com>
2026-05-17 17:20:59 +0545

    fix: null addition setting

--- app/DTO/Settings/BrandingDTO.php
+++ app/DTO/Settings/BrandingDTO.php
@@ -7,26 +7,25 @@ use App\DTO\Common\ImageDTO;
 class BrandingDTO
 {
     public function __construct(
-        public readonly ImageDTO $logoLight,
-        public readonly ImageDTO $logoDark,
-        public readonly ImageDTO $favicon,
+        public readonly ?ImageDTO $logoLight,
+        public readonly ?ImageDTO $logoDark,
+        public readonly ?ImageDTO $favicon,
     ) {}
 
     // $model is your SettingModel instance
     public static function fromModel($model): self
     {
         return new self(
-            // map your actual DB column names here
             logoLight: ImageDTO::make(
-                $model->logo_light_url,
+                $model->logo_light_url ?? null,
                 $model->logo_light_alt ?? 'Logo - Light Mode'
             ),
             logoDark: ImageDTO::make(
-                $model->logo_dark_url,
+                $model->logo_dark_url ?? null,
                 $model->logo_dark_alt ?? 'Logo - Dark Mode'
             ),
             favicon: ImageDTO::make(
-                $model->favicon_url,
+                $model->favicon_url ?? null,
                 $model->favicon_alt ?? 'Favicon'
             ),
         );
@@ -36,10 +35,10 @@ class BrandingDTO
     {
         return [
             'logo' => [
-                'light' => $this->logoLight->toArray(),
-                'dark'  => $this->logoDark->toArray(),
+                'light' => $this->logoLight?->toArray() ?? [],
+                'dark'  => $this->logoDark?->toArray() ?? [],
             ],
-            'favicon' => $this->favicon->toArray(),
+            'favicon' => $this->favicon?->toArray() ?? [],
         ];
     }
 }
